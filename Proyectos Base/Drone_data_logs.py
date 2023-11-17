de13c508b8ca93d9d6f7306d5cacb87c803a152a
Natalia Rodríguez González <80491340+Nataliarg08@users.noreply.github.com>
2023-11-17 08:14:32 -0600

    Update Drone_data_logs.py

--- Proyectos Base/Drone_data_logs.py
+++ Proyectos Base/Drone_data_logs.py
@@ -8,9 +8,12 @@
 
 ##################################################################################################################################################
 
+#Import standar libraries
 import math
 import time
+import pandas
 
+#Import libraries to communicate with Crazyflie
 import cflib.crtp
 from cflib.crazyflie import Crazyflie
 from cflib.crazyflie.log import LogConfig
@@ -18,16 +21,15 @@ from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
 from cflib.crazyflie.syncLogger import SyncLogger
 from cflib.utils import uri_helper
 
-import pandas
 
 # URI to the Crazyflie to connect to
 uri = uri_helper.uri_from_env(default='radio://0/80/2M/E7E7E7E702')
 
 
-#Array donde se almacenaran los datos recopilados a lo largo de la ejecución del código
+#The array where the recolected data will be stored while the code is running. 
 FINAL_ARRAY = []
 
-####################CALLBACKS para cada configuración###################################################
+####################CALLBACKS for each configuration###################################################
 
 def pos_angCallback(timestamp, data, logconf):
     print('[%d][%s]: %s' % (timestamp, logconf.name, data))
@@ -62,64 +64,66 @@ def anglesRatesCallback(timestamp, data, logconf):
 
     FINAL_ARRAY.append(dic_temp)
 
-#################Fin de funciones de callbacks###########################################
+#################End of callback functions###########################################
 
 
-#Se manda a ejecutar el código donde se almacenan los datos
+#It starts to run the code where the data is stored 
 def run_sequence(scf, logconf_pos_ang, logconf_vel, logconf_quat, logconf_angles_rates):
 
     cf = scf.cf
 
-    #Las configuraciones de los logs se añaden al dron
+    #The configuration of the logs are added to the drone.
     cf.log.add_config(logconf_pos_ang)
     cf.log.add_config(logconf_vel)
     cf.log.add_config(logconf_quat)
     cf.log.add_config(logconf_angles_rates)
 
-    #Se anclan las funciones que funcionarán como callbacks
+    #The functions that are gonaa work as callbacks are anchored.
     logconf_pos_ang.data_received_cb.add_callback(pos_angCallback)
     logconf_vel.data_received_cb.add_callback(velCallback)
     logconf_quat.data_received_cb.add_callback(quaternionCallback)
     logconf_angles_rates.data_received_cb.add_callback(anglesRatesCallback)
 
-    #se inician la recopilación de información para cada configuración
+    #Initiates the recovery of the information for each configuration
     logconf_pos_ang.start()
     logconf_vel.start()
     logconf_quat.start()
     logconf_angles_rates.start()
 
     
-    #Durante 10 segundos se estarán llamando los callbacks correspondientes y almacenando los datos
+    #For a lapse of 10 seconds the corresponding callbacks will be called y the data will be stored.
     time.sleep(10)
 
-    #Se terminan los 10 segundos y se terminan los callbacks
+    #The 10 seconds end and the callbacks end.
     logconf_pos_ang.stop()
     logconf_vel.stop()
     logconf_quat.stop()
     logconf_angles_rates.stop()
 
-    #Con este comando se termina de volar el dron parando los motores
+    #Ends the flying the drone by turning off the motors.
     cf.commander.send_stop_setpoint()
     
 
-    #Se genera el excel con los datos recopilados
+    #Generates an excel worksheet with the data recolected.
     global FINAL_ARRAY
     df = pandas.DataFrame(FINAL_ARRAY)
     df.to_csv("Data.csv")
 
 
 if __name__ == '__main__':
+    # Initialize the low-level drivers
     cflib.crtp.init_drivers()
 
-    # posición del dron cuando inicia y orientación en yaw
+    #Position of the drone when it starts and its orientation in yaw.
     initial_x = 1.5
     initial_y = 3.15
     initial_z = 0.0
-    initial_yaw = 90  # En grados
+    initial_yaw = 90  # Degrees
 
 
-    ###Configuración de los logs##################
-    #####variables de posicion y angulos########################################################
+    ######################################################################################Configuration of the logs########################################################################################
+    
+    ##################Position variables and angles##############################
     lg_stab_pos_ang = LogConfig(name='Stabilizer1', period_in_ms=100)
 
     lg_stab_pos_ang.add_variable('stabilizer.roll', 'float')
@@ -129,7 +133,8 @@ if __name__ == '__main__':
     lg_stab_pos_ang.add_variable('stateEstimate.x', 'float')
     lg_stab_pos_ang.add_variable('stateEstimate.y', 'float')
     lg_stab_pos_ang.add_variable('stateEstimate.z', 'float')
-#############Velocidades en x,y,z#####################################################################
+
+    ##################Speed in x, y and z#########################################
 
     lg_stab_vel = LogConfig(name='Stabilizer2', period_in_ms=100)
     
@@ -137,8 +142,8 @@ if __name__ == '__main__':
     lg_stab_vel.add_variable('stateEstimate.vy', 'float')
     lg_stab_vel.add_variable('stateEstimate.vz', 'float')
 
-###################### QUATERNIONS PART#############################################################################
 
+    ##################QUATERNIONS PART##############################################
     lg_stab_quat = LogConfig(name='Stabilizer3', period_in_ms=100)
     
     lg_stab_quat.add_variable('stateEstimate.qx', 'float')
@@ -146,7 +151,8 @@ if __name__ == '__main__':
     lg_stab_quat.add_variable('stateEstimate.qz', 'float')
     lg_stab_quat.add_variable('stateEstimate.qw', 'float')
 
-############################ Rate de los angulos #######################################################################
+
+    ##################Angle Rate#######################################################
 
     lg_stab_angles_rates = LogConfig(name='Stabilizer4', period_in_ms=100)
     
@@ -154,8 +160,8 @@ if __name__ == '__main__':
     lg_stab_angles_rates.add_variable('controller.r_roll', 'float')
     lg_stab_angles_rates.add_variable('controller.r_yaw', 'float')
 
-    #Conexión con el crazy fly 
+    #Connection with the crazy fly
     with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
 
-        #se ejecuta la secuencia para almacenar LOGS
+        #Executes the sequence to store the logs. 
         run_sequence(scf, lg_stab_pos_ang, lg_stab_vel, lg_stab_quat, lg_stab_angles_rates)
