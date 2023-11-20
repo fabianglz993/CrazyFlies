7a66550b2748880b8b5c34e45ce2324cbe6619c6
Natalia Rodríguez González <80491340+Nataliarg08@users.noreply.github.com>
2023-11-20 17:54:22 -0600

    Update StaticFly_1drone_velocity.py

--- Proyectos Base/StaticFly_1drone_velocity.py
+++ Proyectos Base/StaticFly_1drone_velocity.py
@@ -5,7 +5,6 @@
 # Descripción: Código para mantener un vuelo de un dron, en una altura de 1 metro y una X y Y fijas con control de velocidades
 # Formato de nombrar variables y funciones: Minúsculas y descriptivas
 # Comentado por: Angel Sanchez 
-# Correciones de comentarios por: Natalia Rodríguez González
 
 #  Copyright (C) 2019 Bitcraze AB
 
