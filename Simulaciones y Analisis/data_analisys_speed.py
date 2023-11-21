2ed0d752f50d3a5791c37c2208f20592c4ea2366
Natalia Rodríguez González <80491340+Nataliarg08@users.noreply.github.com>
2023-11-20 18:57:15 -0600

    Update data_analisys_speed.py

--- Simulaciones y Analisis/data_analisys_speed.py
+++ Simulaciones y Analisis/data_analisys_speed.py
@@ -7,21 +7,21 @@
 # Comentado por: David Padilla
 
 ##################################################################################################################################################
-
+#Import libraries
 import os
 import pandas as pd
 
-path = "D:\Downloads\Logs_pruebas" # Crea la ruta para abrir el folder que posteriormente definiremos como dir
-dir = os.listdir( path ) # Abre la dirección definida en path
-df1 = pd.DataFrame() # Crea un dataframe vacio
+path = "D:\Downloads\Logs_pruebas" # Creates the path to open the folder that we will later define as address
+dir = os.listdir( path ) # Opens the address defined in path
+df1 = pd.DataFrame() # Create an empty dataframe
 
-for file in dir: # Itera en los diferentes archivos guardados dentro de la carpeta dir
-    df = pd.read_csv(path + r'\\' + file, index_col = 0)  # Se leen los diferentes archivos .csv y los convierte a un Dataframe de pandas  
-    df1 =df1.append(df,ignore_index = True) # Se agregan los dataframes en cada iteración al dataframe que definimos antes del ciclo for
+for file in dir: # Iterates over the different files saved within the dir folder
+    df = pd.read_csv(path + r'\\' + file, index_col = 0)  # The different .csv files are read and converted to a Pandas Dataframe 
+    df1 =df1.append(df,ignore_index = True) # The dataframes are added in each iteration to the dataframe that we defined before the for loop
 
-print('La velocidad máxima en x es:', df1['stateEstimate.vx'].max()) # Despliega en terminal el valor máximo de la velocidad en el eje X [m/s]
-print('La velocidad máxima en y es:', df1['stateEstimate.vy'].max()) # Despliega en terminal el valor máximo de la velocidad en el eje Y [m/s]
-print('La velocidad máxima en z es:', df1['stateEstimate.vz'].max()) # Despliega en terminal el valor máximo de la velocidad en el eje Z [m/s]
-print('El ángulo máximo en yaw es:', df1['stabilizer.yaw'].max()) # Despliega en terminal el valor máximo del ángulo en yaw [°]
-print('El ángulo minimo en yaw es:', df1['stabilizer.yaw'].min()) # Despliega en terminal el valor minimo del ángulo en yaw [°]
+print('La velocidad máxima en x es:', df1['stateEstimate.vx'].max()) # Displays in terminal the maximum value of the speed in the X axis [m/s]
+print('La velocidad máxima en y es:', df1['stateEstimate.vy'].max()) # Displays in terminal the maximum value of the speed in the Y axis [m/s]
+print('La velocidad máxima en z es:', df1['stateEstimate.vz'].max()) # Displays in terminal the maximum value of the speed in the Z axis [m/s]
+print('El ángulo máximo en yaw es:', df1['stabilizer.yaw'].max()) # Displays in terminal the maximum value of the angle in yaw [°]
+print('El ángulo minimo en yaw es:', df1['stabilizer.yaw'].min()) # Displays in terminal the minimum value of the angle in yaw [°]
 
